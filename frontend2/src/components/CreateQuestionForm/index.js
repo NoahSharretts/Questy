a63adf474967a6c0febdf9acd3afb86eb52f7b23
noah <garciasharrettsnoah@gmail.com>
2021-10-14 01:38:36 -0700

    attempting to implement questionForm

--- frontend2/src/components/CreateQuestionForm/index.js
+++ frontend2/src/components/CreateQuestionForm/index.js
@@ -4,4 +4,55 @@ import { useHistory } from 'react-router-dom'
 import { createQuestion, getTopics } from '../../store/question'
 import './CreateQuestionForm.css'
 
-const CreateQuestionForm = ()
+const CreateQuestionForm = () => {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user)
+  const history = useHistory();
+
+  const [body, setBody] = useState('');
+  const [topic, setTopic]
+
+  
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    history.push('/')
+  }
+
+  useEffect(() => {
+    const ERRORS = [];
+  
+    if (!body) ERRORS.push("CANNOT submit an empty question");
+  
+    if (body.length < 3) ERRORS.push("Questions must be 3 characters or more");
+    
+    const fruitName = fruits.map((fruit) => fruit.name)
+    if(fruitName.includes(name)) ERRORS.push(	"Name already exists.")
+
+    if( sweetness < 1 || sweetness > 10) ERRORS.push("Sweetness must be between 1 and 10")
+
+    setErrors(ERRORS)
+  }, [fruits, name, sweetness])
+
+  return (
+    <div className='question-form-container'>
+        <h2>Got a Question?</h2>
+        <form onSubmit={handleSubmit} className='question-form'>
+          <div>
+            <label>
+              What would you like to ask?
+              <input 
+              id='body' 
+              type='text' 
+              onChange={(e) => setBody(e.target.value)} 
+              value={body} />
+            </label>
+          </div>
+          <div>
+
+          </div>
+        </form>  
+    </div>
+  )
+}
